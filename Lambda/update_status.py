9be850c6d26e912581bbb9dff63fae8412da3c51
Aryan_Bhalkar <aryanbhalkar1011@gmail.com>
2026-07-16 02:02:36 +0530

    Create update_status.py

---
+++ Lambda/update_status.py
@@ -0,0 +1,15 @@
+{
+    "message": "Leave status updated successfully",
+    "leave": {
+        "endDate": "2026-07-03",
+        "createdAt": "2026-07-15T19:15:59.822713",
+        "leaveId": "d51bec18-8843-4603-b480-1695e2b7794f",
+        "leaveType": "Sick Leave",
+        "employeeEmail": "aryanbhalkar1011@gmail.com",
+        "documentUrl": "Not uploaded",
+        "reason": "Fever and rest required",
+        "startDate": "2026-07-01",
+        "employeeName": "Aryan Bhalkar",
+        "status": "APPROVED"
+    }
+}
