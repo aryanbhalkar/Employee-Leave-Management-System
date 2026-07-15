b259b3165be2b0299f30efb2d5cb098b8517e8d0
Aryan_Bhalkar <aryanbhalkar1011@gmail.com>
2026-07-16 02:00:57 +0530

    Add leave application data for employee Aryan Bhalkar

---
+++ Lambda/leave_apply.py
@@ -0,0 +1,8 @@
+{
+  "employeeName": "Aryan Bhalkar",
+  "employeeEmail": "aryanbhalkar1011@gmail.com",
+  "leaveType": "Sick Leave",
+  "startDate": "2026-07-01",
+  "endDate": "2026-07-03",
+  "reason": "Fever and rest required"
+}
